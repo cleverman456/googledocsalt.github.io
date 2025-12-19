83cbc7ee1e537c98958e60a726947403fcccf068
cleverman456 <zprice720@gmail.com>
2025-12-19 13:29:57 -0500

    Fix paths in UV configuration
    Updated paths for bundle, config, and service worker.

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -6,7 +6,8 @@ self.__uv$config = {
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
   client: "/uv/uv.client.js",
-  bundle: "/uv/uv.bundle.js",
-  config: "/uv/uv.config.js",
-  sw: "/uv/uv.sw.js",
+  bundle: "./uv/uv.bundle.js",
+  config: "./uv/uv.config.js",
+  sw: "./uv/uv.sw.js",
+
 };
